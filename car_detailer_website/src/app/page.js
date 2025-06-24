4907423c2639f17dda66ae08b822e64adbcb0a8a
Jakub Stec <stecu03@gmail.com>
2025-06-24 12:55:56 +0200

    POPRAWKA ANALITYKI

--- car_detailer_website/src/app/page.js
+++ car_detailer_website/src/app/page.js
@@ -6,8 +6,7 @@ import Gallery from "@/components/Home/Gallery"
 import FancyCards from "@/components/Home/FancyCards"
 import Opinions from "@/components/Home/Opinions"
 import Footer from "@/components/Footer"
-import { Analytics } from "@vercel/analytics/next"
-import ColorPicker from "@/components/ColorPicker"
+
 export default function Home() {
   return (
     <main className="flex flex-col gap-16 pb-8">
